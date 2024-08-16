5ee35ef5eee3e50efa3c46359398a407baecf0b6
John Park <john.park@duke.edu>
2024-08-16 16:52:03 -0500

    Homestack screen added

--- components/routes/HomeStackScreen.js
+++ components/routes/HomeStackScreen.js
@@ -1,12 +1,12 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
 
 const HomeStack = createStackNavigator();
 
 export default function HomeStackScreen() {
     return (
         <HomeStack.Navigator>
-            
+            <HomeStack.Screen name = "Home" component = {HomeScreen}/>
         </HomeStack.Navigator>
     )
 }
