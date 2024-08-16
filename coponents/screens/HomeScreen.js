e979a17032cc807b99dfd2dc8b87e1d04fc10f14
John Park <john.park@duke.edu>
2024-08-16 16:30:46 -0500

    added screens folder

---
+++ coponents/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+export default function HomeScreen() {
+    return (
+        <>
+            <Text>Home</Text>
+        </>
+    )
+}
